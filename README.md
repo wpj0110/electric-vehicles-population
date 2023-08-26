8546428119007b94fa5df9dc8059a407de36e9af
William Javier <wpj0110@gmail.com>
2023-08-26 00:02:19 -0500

    test run on using json data

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# From Data.WA.gov
+## From Data.WA.gov
 
 https://data.wa.gov/Transportation/Electric-Vehicle-Population-Data/f6w7-q2d2
 
-API Endpoint used:
+### API Endpoint used:
 https://data.wa.gov/resource/f6w7-q2d2.json
 
-### Check out https://catalog.data.gov/dataset for more data sets!
+ Check out https://catalog.data.gov/dataset for more data sets!
+
+## Libraries used:
+
+Recharts https://recharts.org/en-US/guide/installation
+
--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import jsonData from './files/ElectricVehiclePopulationData.json'
 
 function App() {
+
+  const data = jsonData;
+  const fieldNameMap = getFieldNames();
+
+
+  console.log(fieldNameMap);
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
+
+  function getCarCountByState(data) {
+    const vehicleData = data;
+    const phevCountString = "phevCount";
+    const evCountString = "evCount";
+  
+    let stateMap = new Map();
+    vehicleData.forEach(row => {
+      const state = row[11];
+      const isPHEV = row[16] === "Plug-in Hybrid Electric Vehicle (PHEV)";
+      if (stateMap.has(state)) {
+        let phevCount = stateMap.get(state)[phevCountString];
+        let evCount = stateMap.get(state)[evCountString];
+        if (isPHEV) {
+          phevCount = stateMap.get(state)[phevCountString] + 1;
+        } else {
+          evCount = stateMap.get(state)[evCountString] + 1;
+        }
+        const value = {
+          evCount: evCount,
+          phevCount: phevCount
+        }
+        stateMap.set(state, value)
+      } else {
+        let evCount = 0;
+        let phevCount = 0;
+        if (isPHEV) {
+          phevCount += 1;
+        } else {
+          evCount += 1;
+        }
+        const value = {
+          evCount: evCount,
+          phevCount: phevCount
+        }
+        stateMap.set(state, value)
+      }
+    });
+
+    return stateMap;
+  }
+
+  function getFieldNames() {
+    const fieldMap = new Map();
+    jsonData.meta.view.columns.forEach((column, index) => fieldMap.set(column["fieldName"], index));
+    return fieldMap;
+  }
+
 }
 
 export default App;
